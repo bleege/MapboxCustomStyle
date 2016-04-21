8f72ee258626808a8c479d439a81173bca28df8c
Brad Leege <bleege@gmail.com>
2016-04-21 14:14:03 -0500

    Initial Commit

---
+++ MapboxCustomStyle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapboxCustomStyle
+//
+//  Created by Brad Leege on 4/21/16.
+//  Copyright © 2016 Mapbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapboxCustomStyle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapboxCustomStyle
+//
+//  Created by Brad Leege on 4/21/16.
+//  Copyright © 2016 Mapbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
